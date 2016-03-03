bdbbd5f558a0f130f3b27c0025335a477fc08ee6
sakshyamdahal <dahalsakshyam@gmail.com>
2016-03-03 15:35:23 -0500

    temp converter app

--- TempConverter/ViewController.swift
+++ TempConverter/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        let firstFrame = CGRect(x: 160, y: 240, width:  100, height:  150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blueColor()
+//        view.addSubview(firstView)
+//        
+//        let secondFrame = CGRect(x: 16, y: 24, width:  10, height:  15)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.redColor()
+//        firstView.addSubview(secondView)
+//
+//        
+//        
+//    }
+    
+    
 }
 
